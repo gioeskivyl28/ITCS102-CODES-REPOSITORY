8e614e1f5e1e09d8050b8152f3f910aec9625687
Gio Esquivel <gioboyesquivel123@gmail.com>
2026-08-25 12:08:47 +0800

    Add files via upload

---
+++ activity5.py
@@ -0,0 +1,10 @@
+a = 2
+b = 34
+
+print("The sum of", a,"plus ", b, "is ", a + b)
+print("The difference of", a,"minus", b, "is ", a - b)
+print("The product of", a,"times", b, "is ", a * b)
+print("The quotient of", a,"minus", b, "is ", a / b)
+print("The floor division of", a,"minus", b, "is ", a // b)
+print("The exponentation of", a,"minus", b, "is ", a ** b)
+print("The modulus of", a,"minus", b, "is ", a % b)
