19c6ab32aa556e630948f449af2d08cfe9823fb3
Gio Esquivel <gioboyesquivel123@gmail.com>
2026-09-08 12:44:15 +0800

    Add files via upload

---
+++ Activity9.py
@@ -0,0 +1,26 @@
+x = 7
+y = 5
+salita1 = "bye"
+salita2 = "bai"
+#1
+print(x > y)
+#2
+print(x < y)
+#3
+print(x <= y)
+#4
+print(x >= y)
+#5
+print(x == y)
+#6
+print(x != y)
+#7
+print( salita1 == salita2 )
+#8
+print( salita1 != salita2 )
+
+
+
+
+
+
