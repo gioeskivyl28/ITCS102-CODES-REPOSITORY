8e8c9cebfc877c186241702c044e73f8e1f86231
Gio Esquivel <gioboyesquivel123@gmail.com>
2026-09-10 12:54:05 +0800

    Add files via upload

---
+++ Activity13.py
@@ -0,0 +1,32 @@
+#Write a python program that accepts age as an integer 
+#After determine the age group label of the given age
+#Age group:
+#1-5 > infant
+#6-12 > kid
+#13-19 > teenager
+#20-29 > early adulthood
+#30-45 > adult
+#46-59 > advance adulthood
+#60-150 > senior
+
+age = int(input("Input age --->"))
+
+age >= 1
+if age >= 1 and age <= 5:
+	print("INFANT")
+elif age >= 6 and age <= 12:
+	print("KID")
+elif age >= 13 and age <= 19:
+	print("TEENAGER")
+elif age >= 20 and age <= 29:
+	print("EARLY ADULTHOOD")
+elif age >= 30 and age <= 45:
+	print("ADULT")
+elif age >= 46 and age <= 59:
+	print("ADVANCE ADULTHOOD")
+elif age >= 60 and age <= 100:
+	print("SENIOR")
+
+else:
+	print("INVALID")
+
