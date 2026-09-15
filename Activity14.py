5b0eab3f9510c97eef1830c6536352f9aea62d87
Gio Esquivel <gioboyesquivel123@gmail.com>
2026-09-15 17:33:54 +0800

    Activity for September 15, 2026
    Refactor loan eligibility checks and interest rate calculations based on credit score and income.

--- Activity14.py
+++ Activity14.py
@@ -1,25 +1,30 @@
+#rene
 age = int(input("Enter your age \n-->"))
-is_employed = bool(input("Are you employed? (True/False) \n -->"))
+is_employed = bool(eval(input("Are you employed? (True/False) \n -->")))
 credit_score = int(input("Enter your credit score: \n-->"))
 annual_income = float(input("Enter your annual income \n-->"))
-has_collateral = bool(input("Do you have collateral? (True/False) \n-->"))
+has_collateral = bool(eval(input("Do you have collateral? (True/False) \n-->")))
 
 if age >= 21 and is_employed == True:
-    print("You are eligible to apply for a loan.")
+    print("You are eligible to apply for a loan")
 else:
-    print("You are not eligible to apply for a loan.")
-    if credit_score >= 750: #tier 1
-        print("You have a high credit score")
+    print("Rejected: Fails baseline criteria")
+
+if credit_score >= 750:# Tier 1
+        base_rate = 5.0
         if annual_income >= 100000:
             base_rate = 4.5
-            print("You have a high salary and high credit score your interest rate is", base_rate)
-        else:
-            base_rate = 5.0
-            print("You have a high salary and high credit score your interest rate is", base_rate)   
-    if credit_score >= 600 and credit_score < 750: #tier 2
-        base_rate = 8.0
-        if has_collateral == True:
-            base_rate = (base_rate - 7)
-            elif
-            
+        print("Loyalty discount. Final rate is:", base_rate, "%")
 
+elif credit_score >= 600 and credit_score < 750:# Tier 2
+    base_rate = 8.0
+    if has_collateral == True:
+            base_rate = 7.0
+    elif annual_income < 40000:
+            base_rate = 9.5
+    print("Approved at", base_rate, "%")
+
+else:# Tier 3 
+    if credit_score < 600:
+        print("Rejected: Credit score too low")
+        #bituin ng mindanao
